a892f1e716105cf0257f1ef593d2f220bfdea012
MDAM182 <mdam2345@gmail.com>
2021-06-17 09:16:52 -0400

    update action added

--- app/controllers/api/v1/genres_controller.rb
+++ app/controllers/api/v1/genres_controller.rb
@@ -3,5 +3,16 @@ class Api::V1::GenresController < ApplicationController
   genres = Genre.all
   #render json: genres
   render json: GenreSerializer.new(genres)
-end
+  end
+
+  def update
+    Genre.find(params[:id]).update(genre_params)
+    render json: Genre.find(params[:id])
+ end
+
+ private
+
+ def genre_params
+   params.require(:program).permit(:name)
+ end
 end
