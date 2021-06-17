2dfe8fb832232a6eea6c30e985155c30f4b99410
MDAM182 <mdam2345@gmail.com>
2021-06-17 09:19:06 -0400

    update & destroy actions added to controller/ find_program method created

--- app/controllers/api/v1/programs_controller.rb
+++ app/controllers/api/v1/programs_controller.rb
@@ -1,4 +1,6 @@
 class Api::V1::ProgramsController < ApplicationController
+  before_action :find_program, only: [:update]
+
 
   def index
     programs = Program.all
@@ -9,15 +11,39 @@ class Api::V1::ProgramsController < ApplicationController
   def create
     program = Program.new(program_params)
     if program.save
-      render json: program, status: :accepted
+      render json: ProgramSerializer.new(program), status: :accepted
     else
       render json: {errors: program.errors.full_messages}, status: :unprocessible_entity
     end
   end
 
+  def update
+      @program.update(program_params)
+      if @program.save
+        render json: @program, status: :accepted
+      else
+        render json: { errors: @program.errors.full_messages }, status: :unprocessible_entity
+      end
+    end
+
+  # def update
+  #   Program.find(params[:id]).update(program_params)
+  #   render json: Program.find(params[:id])
+# end
+#
+# def destroy
+#  render json: Program.find(params[:id]).destroy
+# end
+
+
   private
 
   def program_params
     params.require(:program).permit(:title, :favorite_quote, :media_type, :image_url, :genre_id)
+  end
+
+
+  def find_program
+    @program = Program.find(params[:id])
   end
 end
